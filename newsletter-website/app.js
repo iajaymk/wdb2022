ac161dcca56aec6ca1d794cb6ff1c4fd99cf1981
Ajay Kumar MK <ajaykumarmkaju@gmail.com>
2022-06-25 03:04:54 +0530

    Finished newsletter website

--- newsletter-website/app.js
+++ newsletter-website/app.js
@@ -3,11 +3,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-
+const https = require("https");
 
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+ 
+
 
 app.get("/", function(req, res){
     res.sendFile(__dirname+"/signup.html");
@@ -17,9 +19,61 @@ app.post("/",function(req,res){
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
-    console.log(`${firstName}${lastName}${email}`);
+   
+    
+    const data ={
+        members: [
+            {
+                email_address: email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url ="https://us12.api.mailchimp.com/3.0/lists/bf83a76e84";
+
+    const options = {
+        method: "POST",
+        auth:"ajay1:857416141ffcde0d06b1cacd598247b9-us12"
+    }
+
+    const request = https.request(url, options, function(response){
+
+        if (response.statusCode===200){
+            res.sendFile(__dirname+"/success.html");
+        }
+        else{
+            res.sendFile(__dirname+"/failure.html");
+        }
+
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
+
 })
 
-app.listen(3000,function(){
-    console.log("Server running on port 3000");
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
 })
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+})
+
+
+// API Key
+// 857416141ffcde0d06b1cacd598247b9-us12
+
+// List Id
+// bf83a76e84.
